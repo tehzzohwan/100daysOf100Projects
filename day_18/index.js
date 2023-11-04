aab7bbbb838d14503515b4c34e3caf0ea46253f0
Bobby Enomate <enomatebobby@.com>
2023-11-04 01:53:58 +0100

    day_18 first commit

---
+++ day_18/index.js
@@ -0,0 +1,14 @@
+const trickBtn = document.getElementById('trick');
+const btnContainer = document.querySelector('.btn-container');
+// setting it initially
+btnContainer.style.flexDirection = 'row';
+
+trickBtn.addEventListener('mouseover', (e) => {
+	const currentDir = btnContainer.style.flexDirection;
+	if(currentDir === 'row') {
+		btnContainer.style.flexDirection = 'row-reverse';
+	} else {
+		btnContainer.style.flexDirection = 'row';
+	}
+})
+
